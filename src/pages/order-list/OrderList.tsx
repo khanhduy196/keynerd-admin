2171a9537da6c45b45504136c611c27eec56db6e
Khanh Duy <khanhduy.nguyen@codelink.io>
2024-04-22 12:45:50 +0700

    fix bug

--- src/pages/order-list/OrderList.tsx
+++ src/pages/order-list/OrderList.tsx
@@ -58,6 +58,7 @@ const OrderList = () => {
     }
     setPaginatedListRequest({
       ...paginatedListRequest,
+      currentPage: 1,
       status: currentOrderStatus,
     });
   }, [tabIndex]);
