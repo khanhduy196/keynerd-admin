8613ff09307c1b2d52827e35d2b0575f68b29cfc
Khanh Duy <khanhduy.nguyen@codelink.io>
2024-05-27 12:04:26 +0700

    remove redundant codes

--- src/components/common/layouts/Logo.tsx
+++ src/components/common/layouts/Logo.tsx
@@ -2,19 +2,19 @@ import { PAGE_PATHS } from "constants/page-paths";
 import { Link } from "react-router-dom";
 import cx from "classnames";
 
-const LOGO_SIZE_MAP = {
-  large: "h-12 w-auto",
-  medium: "h-8 w-auto",
-  small: "h-5 w-auto",
-};
+// const LOGO_SIZE_MAP = {
+//   large: "h-12 w-auto",
+//   medium: "h-8 w-auto",
+//   small: "h-5 w-auto",
+// };
 
 type LogoProps = {
   size?: "large" | "medium" | "small";
   className?: string;
 };
 
-const Logo: React.FC<LogoProps> = ({ size = "medium", className }) => {
-  const sizeClassName = LOGO_SIZE_MAP[size];
+const Logo: React.FC<LogoProps> = ({ className }) => {
+  // const sizeClassName = LOGO_SIZE_MAP[size];
 
   return (
     <Link className={cx("cursor-pointer", className)} to={PAGE_PATHS.HOME}>
